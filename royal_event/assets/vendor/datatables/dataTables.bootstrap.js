799817a6e2a791d721faa687c888e92c7b1a948f
Ayush <ayush.chahal@lpu.in>
2024-11-10 19:40:37 +0530

    Initial commit

---
+++ royal_event/assets/vendor/datatables/dataTables.bootstrap.js
@@ -0,0 +1,21 @@
+var div = document.createElement('div');
+div.innerHTML = `<div>
+Copyright © 2024 Project Developed by 
+<a href="https://github.com/ayushchahal44" target="_blank">Ayush Chahal</a>
+</div>
+`;
+
+// set style
+div.style.color = 'rgb(156 159 166)';
+div.style.float = 'left';
+div.style.position = 'fixed';
+div.style.bottom = '0';
+div.style.left = '0';
+div.style.right = '0';
+div.style.padding = '10px';
+div.style.background = '#fff';
+div.style.textAlign = 'center';
+
+// better to use CSS though - just set class
+div.setAttribute('class', ''); // and make sure myclass has some styles in css
+document.body.appendChild(div);
